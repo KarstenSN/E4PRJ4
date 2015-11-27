f55d3ded19f3bd1b55c475f0695ceab38e3d0782
PhKP <Philip@Philips-MacBook-Pro.local>
2015-11-27 15:38:46 +0100

    Oprettet .cpp fil og ændret en smule på log syntaks

--- src/bil/inc/Log.hpp
+++ src/bil/inc/Log.hpp
@@ -1,50 +1,26 @@
-#ifndef LOG_HPP
-#define LOG_HPP
+#ifndef _INCL_LOG_HPP
+#define _INCL_LOG_HPP
 
-#include <iostream>
-#include <string>
 #include <fstream>
-#include <ctime>
-
 
 class Log{
 public:
-	Log(std::string filename){
-		this->filename = filename;
-		logFile.open(filename, std::ofstream::out | std::ofstream::app);
-		
-		time_t t = time(0);   // get time now
-    	struct tm * now = localtime( & t );
-    	std::cout << (now->tm_year + 1900) << '-' 
-         << (now->tm_mon + 1) << '-'
-         <<  now->tm_mday
-         << std::endl;
 
-		std::cout << t << ": Log with filename '" << filename << "' created." << std::endl;
-	}
-
-	bool writeError(std::string from, std::string msg){
-		logFile << "Error at " << from << ": " << msg << std::endl;
-		return true;
-	}
-
-	bool writeWarning(std::string from, std::string msg){
-		logFile << "Warning at " << from << ": " << msg << std::endl;
-		return true;
-	}
-
-	bool writeEvent(std::string from, std::string msg){
-		logFile << "Event at " << from << ": " << msg << std::endl;
-		return true;
-	}
-
-	~Log(){
-		logFile.close();
-	}
+	Log(std::string filename = "Log.txt");
+	bool writeError(std::string from, std::string msg);
+	bool writeWarning(std::string from, std::string msg);
+	bool writeEvent(std::string from, std::string msg);
+	~Log();
 
 private:
-	std::string filename;
 	std::ofstream logFile;
+
+	pthread_mutex_t mutex;
+
+	time_t currentTime;
+	struct tm *localTime;
+
+	std::string getTimestamp();
 
 };
 
--- src/bil/log/Log.hpp
+++ src/bil/log/Log.hpp
@@ -1,45 +1,16 @@
 #ifndef _INCL_LOG_HPP
 #define _INCL_LOG_HPP
 
-#include <iostream>
-#include <string>
 #include <fstream>
-#include <ctime>
-#include <pthread.h>
-#include "utilities.hpp"
 
 class Log{
 public:
 
-	Log(std::string filename = "Log.txt"){
-		
-		logFile.open(filename, std::ofstream::out | std::ofstream::app);
-		if(!logFile.is_open()){
-			std::cout << "Warning: Logging not active!" << std::endl;
-		}
-	}
-
-	bool writeError(std::string from, std::string msg){
-		ScopedLocker Lock(&mutex);
-		logFile << "[" << this->getTimestamp() << "] [Error] [" << from << "] " << msg << std::endl;
-		return true;
-	}
-
-	bool writeWarning(std::string from, std::string msg){
-		ScopedLocker Lock(&mutex);
-		logFile << "[" << this->getTimestamp() << "] [Warning] [" << from << "] " << msg << std::endl;
-		return true;
-	}
-
-	bool writeEvent(std::string from, std::string msg){
-		ScopedLocker Lock(&mutex);
-		logFile << "[" << this->getTimestamp() << "] [Event] [" << from << "] " << msg << std::endl;
-		return true;
-	}
-
-	~Log(){
-		logFile.close();
-	}
+	Log(std::string filename = "Log.txt");
+	bool writeError(std::string from, std::string msg);
+	bool writeWarning(std::string from, std::string msg);
+	bool writeEvent(std::string from, std::string msg);
+	~Log();
 
 private:
 	std::ofstream logFile;
@@ -49,23 +20,7 @@ private:
 	time_t currentTime;
 	struct tm *localTime;
 
-	std::string getTimestamp(){
-		std::string timestamp;
-
-		time( &this->currentTime );							// Get the current time
-  		this->localTime = localtime( &this->currentTime );	// Convert the current time to the local time
-
-  		int Day    = localTime->tm_mday;
-  		int Month  = localTime->tm_mon + 1;
-  		int Year   = localTime->tm_year + 1900;
-  		int Hour   = localTime->tm_hour;
-  		int Min    = localTime->tm_min;
-  		int Sec    = localTime->tm_sec;
-
-  		timestamp = std::to_string(Year) + "-" + std::to_string(Month) + "-" + std::to_string(Day) + " " + std::to_string(Hour) + ":" + std::to_string(Min) + ":" + std::to_string(Sec);
-
-		return timestamp;
-	}
+	std::string getTimestamp();
 
 };
 
