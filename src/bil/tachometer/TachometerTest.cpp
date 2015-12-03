e2124b87df0f8fe0c768ddcaab61439760c06d1f
Your Name <you@example.com>
2015-12-03 14:59:04 +0100

    Forgot somethign

--- src/bil/tachometer/TachometerTest.cpp
+++ src/bil/tachometer/TachometerTest.cpp
@@ -14,26 +14,26 @@ int main() {
     int FR = 0;
     int RL = 0;
     int RR = 0;
-    std::string FLstr= "FL";
+
     while (1){
         
-    FL = myDist.getDistance(FLstr);
-    //sleep(10);
+    FL = myDist.getDistance("FL");
+    usleep(130000);
     FR = myDist.getDistance("FR");
-    //sleep(10);
+    usleep(130000);
     RL = myDist.getDistance("RL");
-    //sleep(10);
+    usleep(130000);
     RR = myDist.getDistance("RR");
-    //sleep(10);
+    usleep(130000);
     p = myTacho.getVelocity();
-    //sleep(10);
+    //usleep(90000);
     std::cout << "FL: " << FL << std::endl;
     std::cout << "FR: " << FR << std::endl;
     std::cout << "RL: " << RL << std::endl;
     std::cout << "RR: " << RR << std::endl;
     std::cout << "km/t: " << p << std::endl;
-    
-    sleep(1);
+    //sleep(1);
+ 
     }
     return 0;
 }
