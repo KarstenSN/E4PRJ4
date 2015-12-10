c77fd6028acc8c88117bfcb85650052b92fefca3
Philip Krogh-Pedersen <philipkroghpedersen@gmail.com>
2015-12-10 11:02:46 +0100

    Tilføjesle af testdel

--- src/bil/log/main.cpp
+++ src/bil/log/main.cpp
@@ -3,23 +3,19 @@
 #include <thread>
 #include <Log.hpp>
 
+//----------main1----------
 #define NUM_THREADS 10
 
 Log Log;
 
 void writers(int num){
-	
 	std::string msg = "Thread #" + std::to_string(num) + " checking in..";
-
 	Log.writeEvent(__PRETTY_FUNCTION__,msg);
 }
 
-
 int main()
 {
-
-	std::thread myThreads[NUM_THREADS];
-	
+	std::thread myThreads[NUM_THREADS];	
     for(int i = 0; i < NUM_THREADS; i++){
         myThreads[i] = std::thread(writers, i);
     }
@@ -30,3 +26,4 @@ int main()
 	}
 	return 0;
 }
+//----------main2----------
